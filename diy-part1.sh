f775c00ed8b20fa24eeb439704d741aae9422bf9
marsnego <marsnego@gmail.com>
2021-02-02 17:45:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ !/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -13,5 +13,5 @@
   Uncomment a feed source
  sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-  Add a feed source
- sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
